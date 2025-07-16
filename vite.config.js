bd3eea22540ee0437907c7b5971e20431e9a4628
zinlinnaung.bit <zinlinnaung.bit@gmail.com>
2025-07-16 14:32:37 +0630

    uploaded

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3011,
+  },
+  preview: {
+    allowedHosts: ["megawecare-app.tharapa.ai"], // Add your allowed host here
+  },
+});
